9761ebee1f1ea986d2640cf72cb701544aa98af0
asheswook <wookboy00@naver.com>
2023-02-18 17:17:39 +0900

    Build: Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='face-classification',
+    version='0.1',
+    author='Jaewook Lee',
+    author_email='me@jwlee.xyz',
+    description='Face Classification package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/asheswook/face-classification',
+    project_urls={
+        'Bug Tracker': 'https://github.com/asheswook/face-classification/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    package=setuptools.find_packages(where='src'),
+    python_requires='>=3.6',
+)
