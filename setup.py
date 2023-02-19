15968e51fb7460a5d8b57913241ea9b81f63ef49
asheswook <wookboy00@naver.com>
2023-02-19 18:59:25 +0900

    Chore: update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setuptools.setup(
     name='FaceFlow',
-    version='0.1',
+    version='0.1.1',
     author='Jaewook Lee',
     author_email='me@jwlee.xyz',
     description='Face Classification package',
