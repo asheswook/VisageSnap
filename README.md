aa430145dc439f66db17ab5d9685641035516f72
asheswook <wookboy00@naver.com>
2023-02-19 18:50:45 +0900

    Chore: change directory and file name again

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# face-classification
+# FaceFlow
 
-Recognizes faces and trains models, brings in the pictures and provides identification predictions. It also performs semi-supervised learning.
+Recognizes faces and trains models, brings in the pictures and provides identification predictions and face classification. It also performs semi-supervised learning.
 
 ## Feature
 
 -   Recognize faces.
 -   Train the model through semi-supervised learning with labeled or unlabeled pictures.
--   It provides identification predictions for pictures.
+-   It provides identification predictions and face classification for pictures
--- setup.py
+++ setup.py
@@ -12,24 +12,24 @@ requirements = [
 ]
 
 setuptools.setup(
-    name='face_classification',
+    name='FaceFlow',
     version='0.1',
     author='Jaewook Lee',
     author_email='me@jwlee.xyz',
     description='Face Classification package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/asheswook/face-classification',
+    url='https://github.com/asheswook/FaceFlow',
     project_urls={
-        'Bug Tracker': 'https://github.com/asheswook/face-classification/issues',
+        'Bug Tracker': 'https://github.com/asheswook/FaceFlow/issues',
     },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'face_classification'},
-    package=setuptools.find_packages(where='face_classification'),
+    package_dir={'': 'FaceFlow'},
+    package=setuptools.find_packages(where='FaceFlow'),
     python_requires='>=3.7',
     install_requires=requirements,
 )
