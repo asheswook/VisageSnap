b151c14de173dd78755d59c2dbda9a50db3e48e2
Jaewook Lee <wookboy00@naver.com>
2023-02-24 18:53:18 +0900

    Chore: update package version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name='VisageSnap',
-    version='0.1',
+    version='0.2',
     author='Jaewook Lee',
     author_email='me@jwlee.xyz',
     description='Face Classification package',
