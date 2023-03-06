385b48c1d93def1c5a97e0960952b9e5d9c328fc
asheswook <wookboy00@naver.com>
2023-03-06 20:19:01 +0900

    Fix: cannot generate NDArray

--- VisageSnap/utils.py
+++ VisageSnap/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def gen(target: list[any]) -> any:
     """
     This function is a generator.
@@ -6,6 +8,6 @@ def gen(target: list[any]) -> any:
     ----------
     target (list) : target list.
     """
-    assert isinstance(target, list), "target must be a list."
+    assert isinstance(target, list | np.ndarray), "target must be a list or numpy.ndarray."
     for i in target:
         yield i
